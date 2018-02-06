47040722b47ae51ecd9f6dab35788cb1891c5c41
Sawan Singh Mahara <ssmahara96@gmail.com>
2018-02-06 17:04:07 +0530

    Tweaking and testing
    Training.m is the master code which right now only stores all the EEG recordings of a single patient, in a 3D structure
    
    SubjectEEG.m is the function that extracts and loads  multiple EEG files into one large 3D structure, in matlab.

--- MasterCodeOffline/Training/Training.m
+++ MasterCodeOffline/Training/Training.m
@@ -1,3 +1,8 @@
-%filePath='..\..\DataSet\From the Internet\4\subject12\*gdf'; %Location of a subject's SSVEP data collected
-filePath='..\..\DataSet\Old BCI Data\*gdf';
+%% Load trials of one human subject
+% Store all the SSVEP sessions of a person in one location and access all
+% of them from there.
+% X holds all the extracted EEG data
+
+filePath='..\..\DataSet\Old BCI Data\fyp2016data\gdf\*.gdf';
 X=SubjectEEG(filePath);
+
