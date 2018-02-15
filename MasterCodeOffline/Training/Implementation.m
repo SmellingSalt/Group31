e10da24e9cbf069900ce75935d797aae7fa5e49f
Sawan Singh Mahara <ssmahara96@gmail.com>
2018-02-15 10:07:51 +0530

    Adjustments
    Changed the file path to the partitioned training and testing set
    
    Also made the variable names more readable

--- MasterCodeOffline/Training/Implementation.m
+++ MasterCodeOffline/Training/Implementation.m
@@ -3,8 +3,15 @@
 % of them from there.
 % X holds all the extracted EEG data
 
-filePath='..\..\DataSet\From the Internet\4\subject12\*.gdf';
+filePath='..\..\DataSet\From the Internet\4\subject10\Training GDF\*.gdf';
 %C:\Users\Prashanth HC\Desktop\Group31\DataSet\From the Internet\4\subject12
 X=SubjectEEG(filePath); % extracts the eeg data from all gdf files in the folder
+
+
+%% Covariance Matrix
+% The covariance matrix is constructed here and then the center of the
+% clusters is also computed. The tangent space projection of the matrices
+% are also obtained
 C=EEGtoCov(X); % compute covariance matrix of each trail and store in corresponding location
-[SubjectMean feat]=CovMean(C); % returns the Cluster center/mean Covariance matrix of the subject
+[ClassMean, TanSpace]=CovMean(C); % returns the Cluster center/mean Covariance matrix of the subject
+
