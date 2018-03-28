b614855d4f6206de587e087501d1552cb298f20c
Sawan Singh Mahara <ssmahara96@gmail.com>
2018-03-28 22:27:52 +0530

    EEG Epoch Extraction Finished
    These functions take GDF files and arranges them as D Epochs per row, until all the samples are used up.

--- MasterCodeOffline/Training/Functions/OnlineFunctions/ExEEG.m
+++ MasterCodeOffline/Training/Functions/OnlineFunctions/ExEEG.m
@@ -2,19 +2,17 @@
 %The Row contains all the D Epochsa and the columns contain remaining D
 %Epochs 
 %Note:MegaEpoch is the set of D Epochs
-%function[X,hand]= ExEEG(s,h,window)
-[s,h]=sload('..\..\..\..\DataSet\Old BCI Data\fyp2016data\gdf\Indra\ssvep-record-train-indra-3-[2016.03.31-23.42.46].gdf'); %Possible inputs {classes,s,h}
-w=2.3*h.SampleRate;          %Window Length in 'number of samples'
-dn=0.2*h.SampleRate;         %Window Spacing in 'number of samples'
-D=10;                        %Number of Epochs
-window=[w, dn, D];
+function[X,hand]= ExEEG(s,h,window)
+% [s,h]=sload('..\..\..\..\DataSet\Old BCI Data\fyp2016data\gdf\Indra\ssvep-record-train-indra-3-[2016.03.31-23.42.46].gdf'); %Possible inputs {classes,s,h}
+% w=2.3*h.SampleRate;          %Window Length in 'number of samples'
+% dn=0.2*h.SampleRate;         %Window Spacing in 'number of samples'
+% D=10;                        %Number of Epochs
+% window=[w, dn, D];
 %Above line is to debug, ignore it
-%window(1,1:2)=window(1,1:2)*h.SampleRate;
+window(1,1:2)=ceil(window(1,1:2)*h.SampleRate);
 w=window(1,1);
 dn=window(1,2);
 D=window(1,3);
-events=h.EVENT.TYP(:,1); 
-samples=h.EVENT.POS(:,1);
 BigWindow=w+(D-1)*dn;                                 %This is the number of samples D Epochs would require, or the window size of a mega epoch
 %% Padding Zeros
 % There will be DD=1+(N-BigWindow)/(D*dn) number of mega overlapping Epochs in the
@@ -23,27 +21,27 @@ BigWindow=w+(D-1)*dn;                                 %This is the number of sam
 % (Which is not possible to construct)
 N=length(s);
 DD=1+(N-BigWindow)/(D*dn);                            %D*dn is the spacing between megaepochs
-excess=BigWindow+(ceil(DD)-1)*dn*D;                   %Now that we have the number of megaepochs, we calculate the number of samples required to get the nearest whole number of megaepochs
+excess=BigWindow+(ceil(DD)-1)*(dn*D);                 %Now that we have the number of megaepochs, we calculate the number of samples required to get the nearest whole number of megaepochs
 excess=excess-N;                                      %Excess is now just the difference between the larger calculated length from the above line and the original length of the signal, 'N'  
 s=[s; zeros(excess,h.NS)];                            %Concatenating zeroes to the end, in all the NS number of channels
 N=length(s);                                          %Updating the length of 's'
 DD=1+(N-BigWindow)/(D*dn);                            %DD is the number of large windows the entire GDF File is divided into, again updated and will always be a whole number.
-hand=zeros(DD,2);                                     %Handle, returning the sample start and ends of all megaEpochs
+                                    %Handle, returning the sample start and ends of all megaEpochs
 %% Filling the cell
 % D epochs in each Row, and DD number of Rows
   sampl1=1;                                           %Initialising start sample as sample number 1.
-  sampl2=w;                                           %Initialising the end of the first epoch as the length of the entire window
+  sampl2=w;                                           %Initialising the end of the first epoch as the length of the entire epoch
    for i=1:DD
-       hand(i,1)=sampl1;
+       
    for j=1:D   
        if sampl2>N
            break;
         end
        X{i,j}=s(sampl1:sampl2,:);
+       hand{i,j}=[sampl1,sampl2]./h.SampleRate;
        sampl1=sampl1+dn;
        sampl2=sampl2+dn;
-       
    end
-   hand(i,2)=sampl2;
+   
   end
-%X1=Bank(X,1);
+%X=Bank(X,1);
---
+++ MasterCodeOffline/Training/Functions/OnlineFunctions/SubEEG.m
@@ -0,0 +1,24 @@
+%This function returns a structure that has all the EEG data of different
+%sessions stored in a 3D struct
+%window is a variable that holds [window length ; window gap] 
+function [X,hand]=SubEEG(filePath,window)
+%% File Location
+% This section gets the names of the gdf EEG files that are stored in a particular location giving the 'name' and 'path' cell structures
+% which holds the names of all the gdf files found in that path.
+fileHandle=dir(filePath); %This is a structure that has all the file names with the .gdf extention in the path specified above
+name={fileHandle.name}; %The name of the file
+path={fileHandle.folder};%The folder where the file is stored
+
+%% Collecting
+% This part collects all the EEG data from different trials and stores it
+% in X
+path1=path{1,1}; %Since we are looking at only one folder at a time, this will not change
+
+for i=1:length(name)
+    name1=name{1,i};    %Iterates over all the file names
+    [s,h]=sload([path1, '\',name1]); %Loads the particular gdf file into s and h
+    [tempx,temph]=ExEEG(s,h,window);
+    X{:,:,i}=tempx;
+    hand{i,:}=temph;
+end
+
