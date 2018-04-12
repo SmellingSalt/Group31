47e8f735f1bdc503129420d4f99015847011d5fb
Sawan Singh Mahara <ssmahara96@gmail.com>
2018-04-12 22:02:32 +0530

    Improvements
    Using different datasets and means.

--- MasterCodeOffline/Training/Algorithm3.m
+++ MasterCodeOffline/Training/Algorithm3.m
@@ -11,7 +11,7 @@
 
 %Mean0-Session21 is the collection of class centers for 0Hz,13Hz, 17Hz
 %and 21Hz respectively
-TrainPath='..\..\DataSet\From the Internet\4\subject10\Algo3\*.gdf';
+TrainPath='..\..\DataSet\From the Internet\4\subject10\Training GDF\*.gdf';
 w=2.6;                                      %Window Length
 dn=0.2;                                     %Window Spacing
 D=5;                                        %Number of Epochs
@@ -36,25 +36,23 @@ window=[w, dn, D];                          %The window parameters
 %% Covariances
 % Computing Covariance of data and also loading the different class centers
 % from offline algorithm, along with the timestamps of original EEG data from offline algorithm.
-
 Cr=EEGtoCov(Xr);                                    %Raw covariance matrix of the entire session
-Cr=OutlierRemoval(Cr,'riemann','riemann');
-%Cf=EEGtoCov(Xfilter);                               %Band Pass Filtered data (Loaded as X)
-
+%Cf=EEGtoCov(Xfilter);                              %Band Pass Filtered data (Loaded as X)
 load('Variables/ClassMean.mat');                    %From Algorithm 1,2 the centers of the raw EEG
-load('Variables/ClassMeanOutlier.mat');             %From Algorithm 1,2 the centers of the outlier removed signal
+load('Variables/ClassMeanOutlier1.mat');             %From Algorithm 1,2 the centers of the outlier removed signal
 load('Variables/timestamp.mat');                    %From Algorithm 1,2 the time stamps of when the signals occured in each session
-
-load('Variables/SimpleCenter.mat');                    %From Algorithm 1,2 the time stamps of when the signals occured in each session
-load('Variables/SimpleCenterOut.mat');                %From Algorithm 1,2 the time stamps of when the signals occured in each session
+load('Variables/SimpleCenter.mat');                 %From Algorithm 1,2 the time stamps of when the signals occured in each session
+load('Variables/SimpleCenterOut.mat');              %From Algorithm 1,2 the time stamps of when the signals occured in each session
 
 time=timestamp(:,:,3);
+%Cr=OutlierRemoval(Cr,'riemann','riemann',ClassMean);
+
 %% Vector of Predictions
 %K holds the predictions of each epoch
-Krn=Prediction(SimpleCenter,Cr,'riemann');                    %Predictions, of raw data
+Krn=Prediction(ClassMean,Cr,'riemann');           %Predictions, of raw data
 %Kfn=Prediction(ClassMean,Cf,'riemann');             %Predictions, of filtered data
 %Kfo=Prediction(ClassMeanOutlier,Cf,'riemann');      %Predictions, of filtered data using outlier removed mean
-Kro=Prediction(SimpleCenterOut,Cr,'riemann');      %Predictions, of raw data using outlier removed mean
+%Kro=Prediction(SimpleCenterOut,Cr,'riemann');       %Predictions, of raw data using outlier removed mean
 
 %% Visualising Predictions
 % Just for visualising the predictions of the algorithm, not needed 
