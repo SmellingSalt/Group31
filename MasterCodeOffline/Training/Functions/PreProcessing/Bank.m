c8bfb1c3977d1612118cf1137416cdf8eb1767d4
Sawan Singh Mahara <ssmahara96@gmail.com>
2018-04-12 22:07:47 +0530

    IIR filters

--- MasterCodeOffline/Training/Functions/PreProcessing/Bank.m
+++ MasterCodeOffline/Training/Functions/PreProcessing/Bank.m
@@ -9,17 +9,17 @@ for i=1:len
        [~, bred]=size(temp1); 
         
        for k=1:bred
-        temp1(:,k)=Band17(temp1(:,k));    %BandPass filtering each column of temp
-        temp2(:,k)=Band13(temp2(:,k));   %BandPass filtering each column of temp
-        temp3(:,k)=Band21(temp3(:,k));   %BandPass filtering each column of temp
-        temp4(:,k)=Band0(temp4(:,k));   %BandPass filtering each column of temp
+        temp1(:,k)=cheby17_1(temp1(:,k));   %BandPass filtering each column of temp
+        temp2(:,k)=cheby13_1(temp2(:,k));   %BandPass filtering each column of temp
+        temp3(:,k)=cheby21_1(temp3(:,k));   %BandPass filtering each column of temp
+        temp4(:,k)=cheby0_1(temp4(:,k));    %Low Pass to extract 0 Hz
        end
-     
-       temp=(temp1+temp2+temp3+temp4)/4;
+       t=100;
+       temp=(t*temp1+t*temp2+t*temp3+temp4)/sqrt(sum(3*t^2+1));
       if snrboost==0
             X{i,j}=temp;
         else    
-            X{i,j}=(temp+X{i,j})/2;
+            X{i,j}=(temp+X{i,j})/sqrt(2);
       end
     end
 end
