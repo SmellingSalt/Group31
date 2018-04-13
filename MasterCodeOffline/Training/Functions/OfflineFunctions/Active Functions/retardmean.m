3d14b9fc4d4208df8e1dacf2eb26c25a2665b304
Sawan Singh Mahara <ssmahara96@gmail.com>
2018-04-13 12:56:37 +0530

    Improved to handle outliers
    This is done by ignoring matricies with all zeros

--- MasterCodeOffline/Training/Functions/OfflineFunctions/Active Functions/retardmean.m
+++ MasterCodeOffline/Training/Functions/OfflineFunctions/Active Functions/retardmean.m
@@ -1,11 +1,21 @@
-function [center] = retardmean(B)
+%% Retard Mean
+% This function computes the mean as one of the data points (B) provided. The
+% mean is the datapoint which yields the smallest squared distance with the
+% other points.
 
+function [center] = retardmean(B)
 %% Initialisation
 % Initialising cost and center
 [~,~,len]=size(B);
 center=B(:,:,1);
+j=2;
+while sum(sum(center))==0
+    center=B(:,:,j);
+    j=j+1;
+end
 mincost=distance(center,B(:,:,2),'riemann');
     for j=2:len
+        
         mincost=mincost+(distance(center,B(:,:,j),'riemann'))^2; 
     end
 
@@ -15,9 +25,14 @@ for i=2:len
     test=B(:,:,i);
     cost=0;
     for j=1:len
+        if sum(sum(test))==0 || sum(sum(B(:,:,j)))==0
+            continue;
+        else
          cost=cost+(distance(test,B(:,:,j),'riemann'))^2;
+        end
     end
     if mincost>cost
         center=test;
+        mincost=cost;
     end
 end
