517c8509d4f964f44df5a5c2dbe6eb21933f4ac1
Sawan Singh Mahara <ssmahara96@gmail.com>
2018-04-14 20:00:55 +0530

    New mean is used

--- MasterCodeOffline/Training/Curve.m
+++ MasterCodeOffline/Training/Curve.m
@@ -3,7 +3,7 @@ a='riemann';
 for i=1:depth
     for j=1:len
         Crow=Cr(j,:,i);
-        [temp1,temp2]=Differential(SimpleCenter,Crow,D,a);         %Each column is for a GDF File, each row is the reult of the Differential of the epoch.
+        [temp1,temp2]=Differential(ClassMean,Crow,D,a);         %Each column is for a GDF File, each row is the reult of the Differential of the epoch.
         result{j,i}=temp1;
         sums{j,i}=temp2;
     end
